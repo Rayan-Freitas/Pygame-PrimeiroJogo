1af368dd629a136d540c81391e38a52fab7cd784
Rayan-Freitas <rayan.freiitas@gmail.com>
2021-10-19 00:26:01 -0300

    Commit 52:00

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ pygame.display.set_caption("Primeiro Jogo")
 
 FPS = 60
 
+VELOCITY = 3
+
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+BORDER = pygame.Rect(WIDTH/2 - 5, 0, 10, HEIGHT)
 
 SPACESHIP_HEIGHT, SPACESHIP_WIDTH = 55, 40
 
@@ -28,9 +33,30 @@ SPACE_BACKGROUND2 = pygame.image.load(os.path.join('Assets', 'spacebg2.gif'))
 
 def draw_window(p1, p2):
     WIN.fill(WHITE)
+    pygame.draw.rect(WIN, BLACK, BORDER)
     WIN.blit(SPACESHIP_P1, (p1.x, p1.y))
     WIN.blit(SPACESHIP_P2, (p2.x, p2.y))
     pygame.display.update()
+
+def p1_movement(keys_pressed, p1):
+    if keys_pressed[pygame.K_a] and p1.x - VELOCITY > 0: # Esquerda
+        p1.x -= VELOCITY
+    if keys_pressed[pygame.K_d] and p1.x + VELOCITY + p1.width < BORDER.x: # Direita
+        p1.x += VELOCITY
+    if keys_pressed[pygame.K_w] and p1.y - VELOCITY > 0: # Cima
+        p1.y -= VELOCITY
+    if keys_pressed[pygame.K_s]  and p1.y + VELOCITY + p1.width < HEIGHT - 15: # Baixo e verificação de borda
+        p1.y += VELOCITY
+
+def p2_movement(keys_pressed, p2):
+    if keys_pressed[pygame.K_LEFT] and p2.x - VELOCITY > BORDER.x + BORDER.width: # Esquerda
+        p2.x -= VELOCITY
+    if keys_pressed[pygame.K_RIGHT] and p2.x + VELOCITY + p2.width < WIDTH: # Direita
+        p2.x += VELOCITY
+    if keys_pressed[pygame.K_UP] and p2.y - VELOCITY > 0: # Cima
+        p2.y -= VELOCITY
+    if keys_pressed[pygame.K_DOWN] and p2.y + VELOCITY + p2.width < HEIGHT - 15: # Baixo e verificação de borda
+        p2.y += VELOCITY
 
 def main():
     p1 = pygame.Rect(100, 200, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
@@ -47,7 +73,10 @@ def main():
         #RANDOM_NUMBER = random.randint(0,255)
         #random_N = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-        p1.x += 1
+        keys_pressed = pygame.key.get_pressed()
+        p1_movement(keys_pressed, p1)
+        p2_movement(keys_pressed, p2)
+
         draw_window(p1, p2)
 
     pygame.quit()
